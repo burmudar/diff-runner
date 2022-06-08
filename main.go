92c00155ec1c0fbd768a0560a6f94d36abaa1286
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-06-08 09:41:04 +0200

    better git file changed command

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -11,15 +12,24 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/urfave/cli/v2"
+
 	"github.com/sourcegraph/run"
 )
 
-func main() {
-	ctx := context.Background()
+var Version string = "DEV"
+var Commit string = "-"
+
+func discoverDirs(ctx *cli.Context) ([]string, error) {
+	mainBranch := ctx.Args().First()
+	if mainBranch == "" {
+		mainBranch = "main"
+	}
 
 	dirs := make([]string, 0)
-	err := run.Cmd(ctx, "git", "diff --stat --name-only").Run().
-		Map(func(ctx context.Context, line []byte, dst io.Writer) (int, error) {
+	// TIL: git whatchanged --name-only --pretty="" origin..HEAD
+	err := run.Cmd(ctx.Context, "git", fmt.Sprintf("diff --name-only %s...", mainBranch)).Run().Map(
+		func(ctx context.Context, line []byte, dst io.Writer) (int, error) {
 			dir := filepath.Dir(string(line))
 			return dst.Write([]byte(dir))
 		}).StreamLines(func(line []byte) {
@@ -32,6 +42,7 @@ func main() {
 	}
 
 	sort.Strings(dirs)
+
 	visited := make(map[string]interface{}, 0)
 	testDirs := make([]string, 0)
 
@@ -56,11 +67,58 @@ func main() {
 		}
 	}
 
+	return testDirs, nil
+}
+
+func runTests(ctx *cli.Context) error {
+
+	testDirs, err := discoverDirs(ctx)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Running tests for dirs: \n- %s\n", strings.Join(testDirs, "\n- "))
+
+	failed := []string{}
+
 	for _, dir := range testDirs {
-		fmt.Printf("Running tests for dir: %s\n", dir)
-		err := run.Cmd(ctx, "go", "test", "./"+dir+"/...").Run().Stream(os.Stdout)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+		_ = run.Cmd(ctx.Context, "go", "test", "./"+dir+"/...").Run().StreamLines(func(line []byte) {
+			if bytes.Contains(line, []byte("FAIL")) {
+				failed = append(failed, dir)
+			}
+
+			fmt.Fprintln(os.Stdout, string(line))
+		})
+	}
+
+	if len(failed) > 0 {
+		os.Exit(1)
+	}
+
+	return nil
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "diff-runner",
+		Usage: "run it in a go git repository and it will run go tests based on the diff",
+		Action: func(ctx *cli.Context) error {
+			return runTests(ctx)
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "version",
+				Usage: "print the version of this tool",
+				Action: func(c *cli.Context) error {
+					fmt.Println("version:", Version)
+					fmt.Println("commit:", Commit)
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
